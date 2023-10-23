196a8a077b456acb44cc20f757f61918b4e867fb
Pedro-A-D-S <pedroalves0409@gmail.com>
2023-10-23 18:32:31 -0300

    feat: :zap: creating crflayer

---
+++ src/crflayer.py
@@ -0,0 +1,32 @@
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import backend as K
+import tensorflow as tf
+
+
+class CRFLayer(Layer):
+    '''
+    Computes the log likelihood during training
+    Performs Viterbi decoding during prediction.
+    '''
+
+    def __init__(
+        self,
+        label_size,
+        mask_id: int = 0,
+        trans_params=None,
+        name: str = 'crf',
+        **kwargs
+    ):
+        super(CRFLayer, self).__init__(name=name, **kwargs)
+        self.label_size = label_size
+        self.mask_id = mask_id
+        self.transition_params = None
+
+        if trans_params is None:
+            self.transition_params = tf.Variable(
+                tf.random.uniform(shape=(label_size, label_size)),
+                trainable=False
+            )
+
+        else:
+            self.transition_params = trans_params
